76e4bafbd869a429ead142105ff8058216581a80
Bjaguar71 <mrym.hanifi@gmail.com>
2023-01-03 19:24:04 +0100

    imported Permissions, ImagePicker and MediaLibrary

--- App.js
+++ App.js
@@ -2,6 +2,11 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button, Image } from "react-native";
 
+// import expo permission and expo image picker
+import * as Permissions from "expo-permissions";
+import * as ImagePicker from "expo-image-picker";
+import * as MediaLibrary from 'expo-media-library';
+
 export default function App() {
   return (
     <View style={styles.container}>
