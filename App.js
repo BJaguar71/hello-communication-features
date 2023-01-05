f907858bbf63e09cfb2c8e2afa8fbdf870abe821
Bjaguar71 <mrym.hanifi@gmail.com>
2023-01-05 18:18:32 +0100

    updated App.js file

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button, Image } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View, Button, Image } from "react-native";
 
 // import expo permission and expo image picker
 import * as Permissions from "expo-permissions";
 import * as ImagePicker from "expo-image-picker";
-import * as MediaLibrary from 'expo-media-library';
+import * as MediaLibrary from "expo-media-library";
+import { Audio } from "expo-av";
 
 export default function App() {
   // initial state
